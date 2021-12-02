39dce51d839b69a2a90bd5e9a0e7fbe3bf63c707
Matheus Gomes <math.s.gomes@gmail.com>
2021-12-02 11:02:04 -0300

    função deletar

--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -11,12 +11,14 @@ export const handleNovoItem = (evento) => {
   
   const calendario = document.querySelector('[data-form-date]');
   const data = moment(calendario.value)
+  const horario = data.format('HH:mm')
   const dataFormatada = data.format('DD/MM/YYYY')
   const concluida = false;
 
   const dados = {
     valor,
     dataFormatada,
+    horario,
     concluida
   }
 
@@ -29,11 +31,11 @@ export const handleNovoItem = (evento) => {
 }
 
 // T maiúsculo para indicar que é um componente.
-export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
+export const Tarefa = ({ valor, horario, concluida }, id) => {
 
   const tarefa = document.createElement('li');
 
-  const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`;
+  const conteudo = `<p class="content">${horario} * ${valor}</p>`;
 
   if(concluida) {
     tarefa.classList.add('done');
@@ -44,7 +46,7 @@ export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
   tarefa.innerHTML = conteudo;
 
   tarefa.appendChild(BotaoConclui(carregaTarefa, id));
-  tarefa.appendChild(BotaoDeleta());
+  tarefa.appendChild(BotaoDeleta(carregaTarefa, id));
   
   return tarefa
 }
